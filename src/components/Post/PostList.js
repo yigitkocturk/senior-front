9f6fd412bf72f25ab0147fd0c4a97820d8db9e04
Tolunay Tezcan <tolunay.tezcan@hepsiburada.com>
2023-01-09 23:31:21 +0300

    Fix post list bug

--- src/components/Post/PostList.js
+++ src/components/Post/PostList.js
@@ -1,45 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import Post from '../Post/Post'
+import React, { useState, useEffect } from 'react';
+import Post from '../Post/Post';
 import PostForm from '../Post/PostForm';
-import "./Post.css";
+import './Post.css';
 
+const PostList = () => {
+  const [isError, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [postList, setPostList] = useState([]);
 
-function PostList(props) {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [postList, setPostList] = useState([]);
+  useEffect(() => {
+    fetch('/posts')
+      .then(res => res.json())
+      .then(
+        result => {
+          setIsLoading(true);
+          setPostList(result);
+        },
+        error => {
+          setIsLoading(true);
+          setError(error);
+        }
+      );
+  }, []);
 
-    useEffect(() => {
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-        fetch("/posts")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setIsLoaded(true);
-                    setPostList(result)
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
-    }, [])
+  if (isError) {
+    return <div> Error!!!</div>;
+  }
 
-    if (error) {
-        return <div> Error!!!</div>
-    } else if (!isLoaded) {
-        return <div>Loading...</div>
-    } else {
-        return (
-            <div >
-                <PostForm key={58} userId={1} userName={1} title={"adsda"} text={"sada"} />
-                {postList.reverse().map(posts => (
-                    <Post key={posts.id} userId={posts.userId} userName={posts.userName} title={posts.title} text={posts.text} date={posts.date} ></Post>
-                ))}
-            </div>
-        )
-    }
+  return (
+    <div>
+      <PostForm
+        key={58}
+        userId={1}
+        userName={1}
+        title={'adsda'}
+        text={'sada'}
+        setPostList={setPostList}
+      />
+      {postList.reverse().map(posts => (
+        <Post
+          key={posts.id}
+          userId={posts.userId}
+          userName={posts.userName}
+          title={posts.title}
+          text={posts.text}
+          date={posts.date}
+        ></Post>
+      ))}
+    </div>
+  );
+};
 
-}
-
-export default PostList
+export default PostList;
